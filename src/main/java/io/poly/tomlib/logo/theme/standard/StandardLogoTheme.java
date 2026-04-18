9a93dd3d56118d7bebfdbdd94cafb72cede1bcd7
Tom Baldwin <tom@polymorphism.co.uk>
2026-04-19 00:29:56 +0100

    Introduce glitch behaviour in `StandardLogoTheme`, including visual distortion, noise, and corruption effects.

--- src/main/java/io/poly/tomlib/logo/theme/standard/StandardLogoTheme.java
+++ src/main/java/io/poly/tomlib/logo/theme/standard/StandardLogoTheme.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import static io.poly.tomlib.logo.theme.easter.EasterTheme.arrayOf;
 
 /// Standard theme for LogoPrinter, providing the default ASCII font logo.
+/// This theme exhibits distinct glitch behaviour, including shearing, corruption, and noise.
 public class StandardLogoTheme extends AbstractTheme {
 
     /// Creates a new instance of StandardLogoTheme.
@@ -22,6 +23,59 @@ public class StandardLogoTheme extends AbstractTheme {
     @Override
     public float getMascotProbability() {
         return 0.01f;
+    }
+
+    @Override
+    public int[] getLogoColour(char c, int row, int col, boolean glitchMode) {
+        if (glitchMode && rnd.nextInt(5) == 0) {
+            // Intense red/white glitches for Standard theme
+            return rnd.nextBoolean() ? new int[]{255, 0, 0} : new int[]{255, 255, 255};
+        }
+        return super.getLogoColour(c, row, col, glitchMode);
+    }
+
+    /// Gets the colour for a star character in the Standard theme.
+    @Override
+    public int[] getStarColour(char c, int row, int col, boolean glitchMode) {
+        if (glitchMode && rnd.nextInt(3) == 0) {
+            return new int[]{255, 0, 0}; // Glitch stars turn red
+        }
+        return super.getStarColour(c, row, col, glitchMode);
+    }
+
+    @Override
+    public void addNoiseColour(StringBuilder colouredLine, int row, int col, char c, boolean glitchMode) {
+        if (glitchMode && rnd.nextInt(4) == 0) {
+            // Occasional red noise for extra corruption feel
+            colouredLine.append(String.format("\u001B[38;2;%d;%d;%dm%c\u001B[0m", 255, 0, 0, c));
+        } else {
+            super.addNoiseColour(colouredLine, row, col, c, glitchMode);
+        }
+    }
+
+    @Override
+    public boolean glitchDistortion() {
+        return true;
+    }
+
+    @Override
+    public boolean glitchCorruption() {
+        return true;
+    }
+
+    @Override
+    public char glitchNoise() {
+        return "░▒▓$@#%&*~!?".charAt(rnd.nextInt(12));
+    }
+
+    @Override
+    public int noiseCount() {
+        return rnd.nextInt(20) + 10; // Even more aggressive noise for Standard theme
+    }
+
+    @Override
+    public String getGlitchMessage() {
+        return "[!] SYSTEM CORRUPTION DETECTED";
     }
 
     @Override
